d4155bdfbb7846ce93df5b3ae84e088ee5bfc0d6
Nuoram953 <69933625+Nuoram953@users.noreply.github.com>
2024-01-20 16:22:06 -0500

    Added search bar for videogame

---
+++ src/app/api.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import { revalidateTag } from "next/cache";
+
+let token: string = "";
+
+const caller = async (method: string, url: string, body: string) => {
+  revalidateTag("token");
+
+  if (!token) {
+    await getToken();
+  }
+
+  const res = await fetch(url, {
+    method,
+    headers: {
+      Accept: "application/json",
+      "Client-ID": `${process.env.IGDB_CLIENT_ID}`,
+      Authorization: `Bearer ${token}`,
+    },
+    body,
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch count");
+  }
+
+  return await res.json();
+};
+
+export async function getToken() {
+  const url = `https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_SECRET}&grant_type=client_credentials`;
+  const res = await fetch(url, {
+    method: "POST",
+    cache: "force-cache",
+    next: { tags: ["token"] },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch token");
+  }
+
+  const data = await res.json();
+  token = data.access_token;
+}
+
+export async function getGameById(id: number) {
+  const url = "https://api.igdb.com/v4/games";
+  const body = `fields aggregated_rating,category,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,platforms,player_perspectives,ports,rating,remakes,remasters,similar_games,slug,standalone_expansions,status,storyline,tags,themes,total_rating,updated_at,url,version_parent,version_title,videos,websites;where id=${id};`;
+
+  return await caller("POST", url, body);
+}
+
+export async function search(searchString: string) {
+  const url = "https://api.igdb.com/v4/search";
+  const body = `fields id, name;search "${searchString}"; limit 50;`;
+
+  return await caller("POST", url, body);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,67 @@
-import Image from 'next/image'
+"use client";
+import { useEffect, useMemo, useState } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import { getGameById, search } from "./api";
+import { debounce } from "@mui/material/utils";
 
 export default function Home() {
+  const [suggestion, setSuggestion] = useState([]);
+  const [searchString, setSearchString] = useState("");
+
+  const debouncedSetter = useMemo(
+    () => debounce((value: string) => onChangeInput(value), 500),
+    [],
+  );
+
+  const onChangeInput = async (value: string) => {
+    if (!value) {
+      return;
+    }
+
+    setSearchString(value);
+
+    const data = await search(value);
+    const modifiedData = data
+      .map((item) => ({
+        ...item,
+        label: item.name,
+      }))
+      .filter(
+        (item, index, array) =>
+          array.findIndex((other) => other.label === item.label) === index,
+      );
+    setSuggestion(modifiedData);
+
+    console.log(data);
+  };
+
   return (
-    <div></div>
-  )
+    <>
+      <h1> Videogame quiz </h1>
+      <Autocomplete
+        id="combo-box-demo"
+        options={suggestion}
+        filterOptions={(x) => x}
+        value={searchString}
+        sx={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Videogame"
+            InputProps={{
+              ...params.InputProps,
+              type: "search",
+            }}
+          />
+        )}
+        onInputChange={(event: any, newValue: any | null) =>
+          debouncedSetter(newValue)
+        }
+        onChange={(event: any, newValue: any | null) => {
+          setSearchString(searchString);
+        }}
+      />
+    </>
+  );
 }
